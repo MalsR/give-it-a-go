8d9232466f3b16019dd302c1c72aac33fdc21a5b
Malinda Rajapakse <malinda_rajapakse@hotmail.com>
2016-12-28 15:07:07 +0000

    Create main.go

---
+++ src/hello/main.go
@@ -0,0 +1,7 @@
+package main
+
+//It is a go convention to name the filename as main.go in a single file go application 
+//all go programs must have a main package and main function which is the entry point for a go program
+
+func main() {
+}
