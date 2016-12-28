a04ab8f432bc03e098d1044185794c230d7b6eab
Malinda Rajapakse <malinda_rajapakse@hotmail.com>
2016-12-28 15:15:53 +0000

    Print message to console

--- src/hello/main.go
+++ src/hello/main.go
@@ -3,5 +3,10 @@ package main
 //It is a go convention to name the filename as main.go in a single file go application 
 //all go programs must have a main package and main function which is the entry point for a go program
 
+//package imports go after the package definitions, import the format package of go
+import "fmt" 
+
 func main() {
+  //prints message to standard out
+  fmt.Println("Planes Rule")
 }
