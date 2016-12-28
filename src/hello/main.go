f7342ef2a73dddc01ed42f3ec09ca40c938e44c9
Malinda Rajapakse <malinda_rajapakse@hotmail.com>
2016-12-28 15:22:01 +0000

    Add several readme steps on compiling and running go programs

--- src/hello/main.go
+++ src/hello/main.go
@@ -7,6 +7,12 @@ package main
 import "fmt" 
 
 func main() {
-  //prints message to standard out
   fmt.Println("Planes Rule")
 }
+
+//to compile the project do 'go build' on root project, in this case 'hello'. This will create 
+//an extention less file hello (run ./hello)
+//to run go programs similar to running like a python 'go run main.go'
+
+//'gofmt main.go' to format go source code
+// run 'gofmt -w main.go' on a file to format the file and write and update source file as opposed to standard output i.r. '-w'
