7934cf38396e3b56d73dd66b54385827d7e74c4c
MalsR <malinda_rajapakse@hotmail.com>
2017-01-03 22:44:49 +0000

    Update hello example to print user input if found

--- src/hello/main.go
+++ src/hello/main.go
@@ -4,14 +4,31 @@ package main
 //all go programs must have a main package and main function which is the entry point for a go program
 
 //package imports go after the package definitions, import the format package of go
-import "fmt" 
+import (
+  "fmt"
+  "os"
+)
 
 func main() {
-  fmt.Println("Planes Rule")
+  // len() built in function that gets the length of some input type
+  if len(os.Args) > 1 {
+    // os.Args returns an array with the 'program arguments, including the name of the go file being executed followed
+    //by any other user input
+    fmt.Println("User input found:", os.Args)
+
+  //  Example of what a command argument looks like when:  go run main.go "Hello Whats the plan?"
+  //  os.Args[0] go run main.go
+  //  os.Args[1] "Hello Whats the plan?"
+    fmt.Println(os.Args[0])
+    fmt.Println(os.Args[1])
+
+  } else {
+    fmt.Println("Planes Rule")
+  }
 }
 
 //to compile the project do 'go build' on root project, in this case 'hello'. This will create 
-//an extention less file hello (run ./hello)
+//an extension less file hello (run ./hello)
 //to run go programs similar to running like a python 'go run main.go'
 
 //'gofmt main.go' to format go source code
