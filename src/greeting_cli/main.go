f1ae52fa278f1d6aa312774f8ae2ae194034692c
MalsR <malinda_rajapakse@hotmail.com>
2017-01-06 00:26:16 +0000

    Add method with multiple returns

--- src/greeting_cli/main.go
+++ src/greeting_cli/main.go
@@ -11,6 +11,7 @@ func main() {
 	userArgs := os.Args
 	fmt.Println("Simple CLI greetings")
 	fmt.Println(userArgs)
+
 	hourOfDay := time.Now().Hour()
 	message, timeError := printHourOfDay(hourOfDay)
 	if timeError != nil {
@@ -27,17 +28,25 @@ func printHourOfDay(hourOfDay int) (string, error) {
 	var message string
 	fmt.Println("Hour of day is", hourOfDay)
 
+	//just to demonstrate a go error
 	if hourOfDay == 23 {
 		timeError := errors.New("Too late to GO... get it?")
 		//message has no value, in go "zero value" is the default value for a data type, in this case "" for string message
 		return message, timeError
 	}
 
-	if hourOfDay < 12 {
-
-	} else if hourOfDay > 12 && hourOfDay < 21 {
-
-	}
 	//nil value tells the calling function that no errors were encountered during execution of this function
-	return "No Errors - Hour of day is", nil
+	if hourOfDay < 12 {
+		return "A very good morning to you folks", nil
+	} else if hourOfDay > 12 && hourOfDay < 19 {
+		return "It's that time of the evening", nil
+	} else if hourOfDay > 19 && hourOfDay < 1 {
+		return "Time to hit the bed", nil
+	} else {
+		return message, errors.New("BAM")
+	}
+}
+
+func exampleMethodReturningMultipleValues() (string, string, error) {
+	return "Hello", "Second Param", nil
 }
